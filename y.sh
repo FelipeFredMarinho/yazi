167d0ff4672b16f6625fa104b1ca4e3307859a34
Felipe Marinho <felipe.fcamarinho@gmail.com>
2026-07-16 00:39:12 -0300

    Add yazi function to auto cd

---
+++ y.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function y() {
+	local tmp cwd
+  tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
